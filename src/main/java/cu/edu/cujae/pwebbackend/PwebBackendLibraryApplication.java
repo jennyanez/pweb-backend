749a803d314b9d4918850b6585fc6007465e30db
Jennifer Yanez <jennyanez0104@gmail.com>
2022-11-06 03:35:03 -0500

    First commit

---
+++ src/main/java/cu/edu/cujae/pwebbackend/PwebBackendLibraryApplication.java
@@ -0,0 +1,13 @@
+package cu.edu.cujae.pwebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwebBackendLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwebBackendLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cu/edu/cujae/pwebbackend/PwebBackendLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package cu.edu.cujae.pwebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwebBackendLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
