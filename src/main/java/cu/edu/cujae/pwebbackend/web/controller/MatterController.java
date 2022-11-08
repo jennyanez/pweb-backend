5388d8df1c4be19dd84cfd117142450b25069a3c
Jennifer Yanez <jennyanez0104@gmail.com>
2022-11-07 21:41:55 -0500

    Matter in Swagger OK

--- src/main/java/cu/edu/cujae/pwebbackend/web/controller/MatterController.java
+++ src/main/java/cu/edu/cujae/pwebbackend/web/controller/MatterController.java
@@ -2,7 +2,12 @@ package cu.edu.cujae.pwebbackend.web.controller;
 
 import cu.edu.cujae.pwebbackend.domain.dto.MatterDto;
 import cu.edu.cujae.pwebbackend.domain.service.MatterService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,29 +22,58 @@ public class MatterController {
     private MatterService matterService;
 
     @GetMapping("/all")
+    /**todo esto es lo que se muestra en el swagger **/
+    @Operation(summary = "Get all matters")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
     public ResponseEntity<List<MatterDto>> getAll(){
         return new ResponseEntity<>(matterService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<MatterDto> getMatter(@PathVariable("id") Long matterId){
+    @Operation(summary = "Get matter by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Matter not found")
+    })
+    public ResponseEntity<MatterDto> getMatter(@Parameter(description = "The id of the Matter")
+                                                   @PathVariable("id") Long matterId){
         return matterService.getMatter(matterId).map(matterDto ->
                 new ResponseEntity<>(matterDto, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/save")
-    public ResponseEntity<MatterDto> saveMatter(@RequestBody MatterDto matterDto){
+    @Operation(summary = "Save a new matter")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
+    public ResponseEntity<MatterDto> saveMatter(@Parameter(description = "The new matter")
+                                                    @RequestBody MatterDto matterDto){
         return new ResponseEntity<>(matterService.saveMatter(matterDto), HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity updateMatter(@RequestBody MatterDto matterDto,
-                                       @PathVariable("id") Long matterId) {
+    @Operation(summary = "Update a matter")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Matter not found")
+    })
+    public ResponseEntity updateMatter(@Parameter(description = "The matter that it's going to be updated")
+                                            @RequestBody MatterDto matterDto,
+                                       @Parameter(description = "The id of the Matter")
+                                            @PathVariable("id") Long matterId) {
         return new ResponseEntity<>(matterService.updateMatter(matterDto, matterId), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity deleteMatter(@PathVariable("id") Long matterId){
+    @Operation(summary = "Delete a matter")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Matter not found")
+    })
+    public ResponseEntity deleteMatter(@Parameter(description = "The id of the Matter")
+                                           @PathVariable("id") Long matterId){
         if(matterService.deleteMatter(matterId)){
             return new ResponseEntity(HttpStatus.OK);
         }
