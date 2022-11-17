647a3dc72bf4775c92cb087b999e06b768f8b85c
Andy Piloto <pilotoandy30@gmail.com>
2022-11-17 00:07:53 -0500

    Dice jenn que to esta okeiii

--- src/main/java/cu/edu/cujae/pwebbackend/web/controller/XUserController.java
+++ src/main/java/cu/edu/cujae/pwebbackend/web/controller/XUserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/xusers")
+@RequestMapping("/xUserList")
 public class XUserController {
 
     @Autowired
