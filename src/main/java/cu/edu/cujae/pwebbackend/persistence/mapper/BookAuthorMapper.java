2133e71df48c1ba0c50669abc32275b6daefb0ba
Jennifer Yanez <jennyanez0104@gmail.com>
2022-11-20 13:49:36 -0500

    commit for push

---
+++ src/main/java/cu/edu/cujae/pwebbackend/persistence/mapper/BookAuthorMapper.java
@@ -0,0 +1,28 @@
+package cu.edu.cujae.pwebbackend.persistence.mapper;
+
+import cu.edu.cujae.pwebbackend.persistence.entity.BookAuthor;
+import cu.edu.cujae.pwebbackend.domain.dto.BookAuthorDto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {BookMapper.class, AuthorMapper.class})
+public interface BookAuthorMapper {
+    @Mappings(
+            {
+                @Mapping(source="book", target = "book"),
+                @Mapping(source = "book.bookId", target = "bookId"),
+                @Mapping(source="author", target = "author"),
+                @Mapping(source = "author.authorId", target = "authorId"),
+            }
+    )
+    BookAuthor toBookAuthor(BookAuthorDto bookAuthorDto);
+    List<BookAuthor> toBookAuthorList(List<BookMapper> bookAuthorDtoList);
+
+    @InheritInverseConfiguration
+    BookAuthorDto toBookAuthorDto(BookAuthor bookAuthor);
+    List<BookAuthorDto> toBookAuthorDtoList(List<BookAuthor> bookAuthorList);
+}
