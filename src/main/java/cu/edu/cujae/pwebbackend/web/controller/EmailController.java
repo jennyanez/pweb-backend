c9473946c2ab9e1143190f43a97d63205a86f8a1
Jennifer Yanez <jennyanez0104@gmail.com>
2022-12-12 23:59:23 -0500

    correo ok

---
+++ src/main/java/cu/edu/cujae/pwebbackend/web/controller/EmailController.java
@@ -0,0 +1,39 @@
+package cu.edu.cujae.pwebbackend.web.controller;
+
+import cu.edu.cujae.pwebbackend.domain.dto.UserDto;
+import cu.edu.cujae.pwebbackend.domain.service.UserService;
+import cu.edu.cujae.pwebbackend.utils.SendEmail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sendmail")
+public class EmailController {
+
+    @Autowired
+    UserService userServices;
+
+    @PostMapping("/{email}")
+    public ResponseEntity<String> sendEmail(@PathVariable String email)
+            throws Exception {
+        SendEmail sendEmail = new SendEmail();
+        int randomPIN = (int) (Math.random() * 90000000) + 1000;
+
+        UserDto user = userServices.getUserByEmail(email);
+        if (user != null) {
+            user.setRecoverCode(Integer.toString(randomPIN));
+            String name = user.getFirstName().split(" ")[0];
+            userServices.save(user);
+            sendEmail.sendEmail(
+                    email,
+                    name,
+                    "Codigo de verificacion",
+                    Integer.toString(randomPIN)
+            );
+            return ResponseEntity.ok("Correo enviado");
+        } else {
+            return ResponseEntity.ok("Correo no registrado");
+        }
+    }
+}
