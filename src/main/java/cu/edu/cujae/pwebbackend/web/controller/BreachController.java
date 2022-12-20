4f49738241003e9200114f1dac1bc53a9321560e
Jennifer Yanez <jennyanez0104@gmail.com>
2022-12-19 22:50:10 -0500

    modifying urls for breach & sanctions

--- src/main/java/cu/edu/cujae/pwebbackend/web/controller/BreachController.java
+++ src/main/java/cu/edu/cujae/pwebbackend/web/controller/BreachController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/breaches")
+@RequestMapping("/api/v1/breaches")
 public class BreachController {
     @Autowired
     private BreachService breachService;
